80267060e3fd557a1f3962f60c1be33e1935fe06
Chris Tse <ctse@rent.com>
2025-02-18 21:51:23 -0600

    more progress on conversion part of script

--- cli/main.go
+++ cli/main.go
@@ -4,19 +4,28 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
+
+	htmltomarkdown "github.com/JohannesKaufmann/html-to-markdown/v2"
 )
 
-// Define a struct to match the expected JSON response
 type GhostResponse struct {
 	Posts  []Post     `json:"posts,omitempty"`
 	Errors []APIError `json:"errors,omitempty"`
 }
 
 type Post struct {
-	Title string `json:"title"`
-	// Add other fields as necessary
+	Title               string `json:"title"`
+	Html                string `json:"html"`
+	Excerpt             string `json:"excerpt"`
+	ReadingTime         int    `json:"reading_time"`
+	PublishedAt         string `json:"published_at"`
+	FeatureImageSrc     string `json:"feature_image"`
+	FeatureImageAlt     string `json:"feature_image_alt"`
+	FeatureImageCaption string `json:"feature_image_caption"`
+	Slug                string `json:"slug"`
 }
 
 type APIError struct {
@@ -26,38 +35,48 @@ type APIError struct {
 }
 
 func main() {
-	fmt.Println("\n📝 Ghost Export to Markdown - Starting up...")
-
-	if os.Getenv("GHOST_API_KEY") != "" {
-		fmt.Println("🔑 `GHOST_API_KEY` detected, using:", os.Getenv("GHOST_API_KEY"))
-	}
-
-	if len(os.Args) < 3 {
-		fmt.Println("Usage: ghost-export-to-markdown --url=<GHOST_URL> --api-key=<GHOST_API_KEY>")
-		os.Exit(1)
-	}
+	fmt.Print("\n📝 Ghost Export to Markdown - Starting up...\n\n")
 
 	apiUrl := flag.String("url", os.Getenv("GHOST_URL"), "Ghost URL")
 	apiKey := flag.String("api-key", os.Getenv("GHOST_API_KEY"), "Ghost Admin API Key")
+	dir := flag.String("dir", "ghost-export", "Directory to save the posts")
 
 	flag.Parse()
 
 	if *apiUrl == "" || *apiKey == "" {
-		fmt.Println("Usage: ghost-export-to-markdown --url=<GHOST_URL> --api-key=<GHOST_API_KEY>")
+		fmt.Println("Usage: ghost-export-to-markdown --url=your.ghost.url --api-key=Your1Ghost2Api3Key")
+		fmt.Println("You can also set GHOST_URL and GHOST_API_KEY as environment variables")
 		os.Exit(1)
 	}
 
-	fetchFromGhostAPI(*apiUrl, *apiKey)
+	fmt.Println("🌐 Using Ghost URL: ", *apiUrl)
+	fmt.Printf("🔑 Using Ghost API Key: %s\n\n", *apiKey)
+
+	fmt.Println("🔍 Fetching posts from Ghost API...")
+	posts, err := fetchFromGhostAPI(*apiUrl, *apiKey)
+
+	if err != nil {
+		log.Fatalln("Error during Ghost API fetch:", err)
+	}
+
+	if err := os.MkdirAll(*dir, 0755); err != nil {
+		log.Fatalln("Error creating directory:", err)
+	}
+
+	for _, post := range posts {
+		savePostToMarkdown(post, *dir)
+	}
+
 	os.Exit(0)
 }
 
-func fetchFromGhostAPI(apiURL, apiKey string) {
-	url := fmt.Sprintf("https://%s/ghost/api/v3/content/posts/?key=%s", apiURL, apiKey)
+func fetchFromGhostAPI(apiURL, apiKey string) ([]Post, error) {
+	url := fmt.Sprintf("https://%s/ghost/api/content/posts/?key=%s", apiURL, apiKey)
+
 	resp, err := http.Get(url)
 
 	if err != nil {
-		fmt.Println("Error fetching from Ghost API:", err)
-		return
+		return nil, err
 	}
 
 	defer resp.Body.Close()
@@ -65,8 +84,7 @@ func fetchFromGhostAPI(apiURL, apiKey string) {
 	var result GhostResponse // Declare the result variable
 
 	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
-		fmt.Println("Error reading response body:", err)
-		return
+		return nil, err
 	}
 
 	// Check for errors in the response
@@ -74,8 +92,34 @@ func fetchFromGhostAPI(apiURL, apiKey string) {
 		for _, apiErr := range result.Errors {
 			fmt.Printf("Error: %s (Code: %s)\n", apiErr.Message, apiErr.Code)
 		}
+		return nil, err
+	}
+
+	return result.Posts, nil
+}
+
+func savePostToMarkdown(post Post, dir string) {
+	// Convert HTML to Markdown
+	markdown, err := htmltomarkdown.ConvertString(post.Html)
+
+	if err != nil {
+		fmt.Printf("Error converting HTML to Markdown: %s\n", post.Slug)
 		return
 	}
 
-	fmt.Println("Response from Ghost API:", result.Posts) // Print the posts
+	// Prepare frontmatter
+	frontmatter := fmt.Sprintf("---\nTitle: %s\nPublished: %s\nTags: %v\n---\n", post.Title, post.PublishedAt, post.Tags)
+
+	// Combine frontmatter and markdown content
+	finalContent := frontmatter + markdown
+
+	// Save to a markdown file
+	fileName := fmt.Sprintf("%s/%s.md", dir, post.Slug)
+
+	err := os.WriteFile(fileName, []byte(finalContent), 0644)
+
+	if err != nil {
+		fmt.Printf("Error saving post %s to file\n: %v", post.Slug, err)
+		return
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module ghost-export-to-markdown
 
 go 1.24.0
+
+require (
+	github.com/JohannesKaufmann/dom v0.2.0 // indirect
+	github.com/JohannesKaufmann/html-to-markdown/v2 v2.2.2 // indirect
+	golang.org/x/net v0.35.0 // indirect
+)
